9ce9cde8a2f0aec5bf46dbe0999941491a9945ab
See way Liang <hi@sihuiliang.com>
2023-07-25 20:15:02 +1000

    chore: align with action default

--- ci.bash
+++ ci.bash
@@ -1,6 +1,6 @@
 echo "set npmrc"
 npm config set always-auth true
-npm config set //npm.pkg.github.com/:_authToken "$GITHUB_TOKEN"
+npm config set //npm.pkg.github.com/:_authToken "$NODE_AUTH_TOKEN"
 npm config set @your-compnay:registry https://npm.pkg.github.com
-yarn npm-cli-login -u seeliang -p "$GITHUB_TOKEN" -e hi@sihuiliang.com -r https://npm.pkg.github.com
+yarn npm-cli-login -u seeliang -p "$NODE_AUTH_TOKEN" -e hi@sihuiliang.com -r https://npm.pkg.github.com
 npm config list 
