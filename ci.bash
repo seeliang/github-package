3d30b42ef40603dbb1e337da0098fe3a89a741ef
See way Liang <hi@sihuiliang.com>
2023-07-25 20:39:38 +1000

    fix: token set

--- ci.bash
+++ ci.bash
@@ -2,6 +2,7 @@ echo "set npmrc"
 npm config set always-auth true
 npm config set //npm.pkg.github.com/:_authToken "$NODE_AUTH_TOKEN"
 npm config set @your-compnay:registry https://npm.pkg.github.com
-npx npm-cli-login -u seeliang -p "$NODE_AUTH_TOKEN" -e hi@sihuiliang.com -r https://npm.pkg.github.com
 npm config list 
+echo "login"
+npx npm-cli-login -u seeliang -p "$NODE_AUTH_TOKEN" -e hi@sihuiliang.com -r https://npm.pkg.github.com
 npm whoami --registry=https://npm.pkg.github.com
