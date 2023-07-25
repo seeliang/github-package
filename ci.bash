405961cd950145857c605e90ac8843002d424b6e
See way Liang <hi@sihuiliang.com>
2023-07-26 08:33:01 +1000

    fix: publish

--- ci.bash
+++ ci.bash
@@ -1,8 +1,10 @@
-echo "set npmrc"
+echo "------------- set npmrc ---------------"
 npm config set always-auth true
 npm config set //npm.pkg.github.com/:_authToken "$NODE_AUTH_TOKEN"
 npm config set @your-compnay:registry https://npm.pkg.github.com
+echo "---------------- cofig ----------------"
 npm config list 
-echo "login"
+echo "---------------- login ----------------"
 npx npm-cli-login -u seeliang -p "$NODE_AUTH_TOKEN" -e hi@sihuiliang.com -r https://npm.pkg.github.com
+echo "----------------- user ----------------"
 npm whoami --registry=https://npm.pkg.github.com
