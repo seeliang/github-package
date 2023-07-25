dbd98f855997f6ef61e17178c1d6472c00d7eb58
See way Liang <hi@sihuiliang.com>
2023-07-25 19:58:43 +1000

    feat: add user in bash

--- ci.bash
+++ ci.bash
@@ -2,4 +2,5 @@ echo "set npmrc"
 npm config set always-auth true
 npm config set //npm.pkg.github.com/:_authToken "$GITHUB_TOKEN"
 npm config set @your-compnay:registry https://npm.pkg.github.com
+yarn npm-cli-login -u seeliang -p "$GITHUB_TOKEN" -e hi@sihuiliang.com -r https://npm.pkg.github.com
 npm config list 
